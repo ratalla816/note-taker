9db1bcfe6ae71e0f4e45e113f616eadc3b4f7a3e
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-09-04 14:58:05 -0500

    routes

--- server.js
+++ server.js
@@ -11,52 +11,29 @@
 const express = require('express');
 const path = require('path');
 const fs = require("fs");
-
-// functions to write and append data
-// function fs.writeFile
-// fs.appendFile
-fs.appendFile( path, data[, options], callback )
-// https://www.geeksforgeeks.org/node-js-fs-appendfile-function/
+const app = express();
+const PORT = 3001;
 
 const app = express();
 const PORT = 3001;
 
-// Sets up the Express app to handle data parsing
+// intercept our POST request before it gets to the callback function
+// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
-fs.appendFile( path, data[, options], callback )
-// https://www.geeksforgeeks.org/node-js-fs-appendfile-function/
-
-
-// Data
-// =============================================================
-// const characters = [
-//   {
-//     routeName: 'yoda',
-//     name: 'Yoda',
-//     role: 'Jedi Master',
-//     age: 900,
-//     forcePoints: 2000
-//   },
-//   {
-//     routeName: 'darthmaul',
-//     name: 'Darth Maul',
-//     role: 'Sith Lord',
-//     age: 200,
-//     forcePoints: 1200
-//   },
-//   {
-//     routeName: 'obiwankenobi',
-//     name: 'Obi Wan Kenobi',
-//     role: 'Jedi Master',
-//     age: 55,
-//     forcePoints: 1350
-//   }
-// ];
+// instructions for testing POST requests: https://courses.bootcampspot.com/courses/715/pages/11-dot-2-4-test-routes-in-insomnia-core?module_item_id=227116
 
 // Routes
 // =============================================================
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(mainDir, "notes.html"));
+});
+
+
 
 // Create an `/add` route that returns `add.html`
 //
@@ -71,18 +48,10 @@ app.get('/api/notes', (req, res) => {
 
 
 
-app.get('/api/notes/:notes', (req, res) => {
-  const chosen = req.params.note;
 
-  console.log(chosen);
 
-  for (let i = 0; i < notes.length; i++) {
-    if (chosen === notes[i].routeName) {
-      return res.json(notes[i]);
-    }
-  }
-
-  return res.json(false);
+app.get("*", function(req, res) {
+  res.sendFile(path.join(mainDir, "index.html"));
 });
 
 app.post('/api/notes', (req, res) => {
@@ -98,8 +67,30 @@ app.post('/api/notes', (req, res) => {
   res.json(newNote);
 });
 
+// functions to write and append data
+// function fs.writeFile
+// fs.appendFile
+fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+res.json(savedNotes);
+
+
+
 // Listener
 // =============================================================
 app.listen(PORT, () => {
   console.log(`App listening on PORT ${PORT}`);
 });
+
+// app.get('/api/notes/:notes', (req, res) => {
+//   const chosen = req.params.note;
+
+//   console.log(chosen);
+
+//   for (let i = 0; i < notes.length; i++) {
+//     if (chosen === notes[i].routeName) {
+//       return res.json(notes[i]);
+//     }
+//   }
+
+//   return res.json(false);
+// });
